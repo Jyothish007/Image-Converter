38f5f1a6f97736c000320a4fe00881f36ed8d805
Jyothish007 <jyothishaji2002@gmail.com>
2026-05-31 20:30:06 +0530

    [Add] proper link

--- frontend/app/components/ImageConverter.tsx
+++ frontend/app/components/ImageConverter.tsx
@@ -63,10 +63,13 @@ export default function ImageConverter() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:8000/process-image", {
-        method: "POST",
-        body: formData,
-      });
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/process-image`,
+        {
+          method: "POST",
+          body: formData,
+        }
+      );
 
       if (!res.ok) {
         const error = await res.json();
